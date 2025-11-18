f2e5c5eaef9837088afa00d3b510363341923cf5
baaamk <brandon55321@gmail.com>
2025-11-19 04:43:17 +0900

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind 설치 성공! 🎉
+      </h1>
+
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg shadow">
+        버튼 테스트
+      </button>
+    </div>
+  );
+}
